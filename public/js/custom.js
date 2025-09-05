2deb5c652d5b6983b312644232ff210009eb8af3
yashzindex <yashzindex@gmail.com>
2025-09-05 15:47:53 +0530

    change in headder

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,71 +1,26 @@
+
 $(document).ready(function () {
   // Menu Button
   let menu = $("#menu-toggle");
   let navMenu = $("#nav-menu");
 
+  // Toggle menu on hamburger click
   menu.click(function () {
     $(this).toggleClass("open");
     $(navMenu).toggleClass("leftZero");
+
+    // Toggle body overflow
+    if ($(navMenu).hasClass("leftZero")) {
+      $("body").css("overflow", "hidden"); // prevent scrolling
+    } else {
+      $("body").css("overflow", ""); // reset to default
+    }
   });
 
-  // animations
-  //   gsap.registerPlugin(ScrollTrigger);
-
-  //   ScrollTrigger.batch(".fade-in", {
-  //     onEnter: (batch) => {
-  //       gsap.to(batch, {
-  //         opacity: 1,
-  //         y: 0,
-  //         filter: "blur(0px)",
-  //         duration: 0.8,
-  //         ease: "power2.out",
-  //         stagger: 0.2,
-  //       });
-  //     },
-  //     start: "top 80%",
-  //     once: true,
-  //   });
-
-  //   ScrollTrigger.batch(".fade-banner-image", {
-  //     onEnter: (batch) => {
-  //       gsap.to(batch, {
-  //         opacity: 1,
-  //         filter: "blur(0px)",
-  //         // transform: " translateZ(10px)",
-  //         duration: 0.9,
-  //         ease: "power2.out",
-  //         stagger: 1,
-  //         delay:0.7,
-  //       });
-  //     },
-  //     start: "top 80%",
-  //     once: true,
-  //   });
-
-  //   ScrollTrigger.batch(".fade-only", {
-  //     onEnter: (batch) => {
-  //       gsap.to(batch, {
-  //         opacity: 1,
-  //         duration: 2,
-  //         ease: "power2.out",
-  //         stagger: 0.7,
-  //       });
-  //     },
-  //     start: "top 80%",
-  //     once: true,
-  //   });
-
-  //   ScrollTrigger.batch(".blur-only", {
-  //     onEnter: (batch) => {
-  //       gsap.to(batch, {
-  //         opacity: 1,
-  //         filter: "blur(0px)",
-  //         duration: 0.8,
-  //         ease: "power2.out",
-  //         stagger: 0.7,
-  //       });
-  //     },
-  //     start: "top 80%",
-  //     once: true,
-  //   });
+  // Close menu when a nav link is clicked
+  $("#nav-menu a").click(function () {
+    menu.removeClass("open"); // reset hamburger state
+    navMenu.removeClass("leftZero"); // hide nav
+    $("body").css("overflow", ""); // allow scrolling again
+  });
 });
